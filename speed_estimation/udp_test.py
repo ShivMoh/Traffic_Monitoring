8c30896528f9c89c79f8c53b10e8f523db0612a7
shivmoh <shiveshmohamed1234@gmail.com>
2024-11-24 22:07:29 -0400

    sdfjsdfjsdf

--- speed_estimation/udp_test.py
+++ speed_estimation/udp_test.py
@@ -1,5 +1,6 @@
 import socket
-
+import json
+from typing import Dict
 
 def send_udp_packet(data):
     UDP_IP = "127.0.0.1"
@@ -9,3 +10,23 @@ def send_udp_packet(data):
     sock = socket.socket(socket.AF_INET, # Internet
                  socket.SOCK_DGRAM) # UDP
     sock.sendto(bytes(MESSAGE, 'utf-8'), (UDP_IP, UDP_PORT))
+
+
+def send_udp_packet_json(data : Dict):
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
+    MESSAGE = json.dumps(dict)
+    
+    print(MESSAGE)
+    
+    sock = socket.socket(socket.AF_INET, # Internet
+                 socket.SOCK_DGRAM) # UDP
+    sock.sendto( bytes(MESSAGE, 'utf-8'), (UDP_IP, UDP_PORT))
+
+dict = {
+        "number_of_cars": "10",
+        "positions": [],
+        "time" : "14:00:00"
+}
+while True:
+    send_udp_packet_json(dict)
