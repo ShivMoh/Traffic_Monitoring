bdce36077c41d2a5aca6eb3a803398f972cb35f8
shivmoh <shiveshmohamed1234@gmail.com>
2025-01-08 19:08:25 -0400

    did something

--- speed_estimation/yolo_native_speed_estimator.py
+++ speed_estimation/yolo_native_speed_estimator.py
@@ -2,6 +2,7 @@ import cv2 as cv
 from ultralytics import YOLO, solutions
 import mss
 import numpy as np
+import math
 
 sct = mss.mss()
 
@@ -11,31 +12,32 @@ names = model.model.names
 
 # boundary_1 = [(330, 380), (330, 450)]
 # boundary_2 = [(1030, 380), (1030, 450)]
-points = [(0, 380), (1920, 380)]
+points = [(int(1920/2), 0), (int(1920/2), 1080)]
 w = 1920
 h = 1080
 fps = 1/25
+speed_region = [(0, 0), (1920, 0), (1920, 1080), (0, 1080)]
 
 speed_obj = solutions.SpeedEstimator(
     reg_pts=points,
     names=names,
-    view_img=True
+    view_img=True,
 )
+
+current_count = 0
 
 while True:
     raw_frame = np.array(sct.grab(bounding_box))
     
     frame = raw_frame[:, :, :3]
-    tracks = model.track(frame, persist=True, show=False)
-    
-    if tracks:
-        speed_frame = speed_obj.estimate_speed(raw_frame, tracks)
+    result = model.track(frame, persist=True, show=False)
+     
+    if result:
+        speed_frame = speed_obj.estimate_speed(raw_frame, result)
         if speed_frame is not None:
-            print("showing frame")
-            cv.imshow("Frame", speed_frame)
+            frame = speed_frame
 
-    # cv.imshow("Speed Frame", np.array(speed_frame))
-
+    cv.imshow("Frame", frame)
 
     if cv.waitKey(25) & 0xFF == ord('q'):
        break 
